f251d9aa225fc711fa4c803ae763b902334b04d7
PedroFariaa <pedrommlcfaria@gmail.com>
2014-04-14 23:50:26 +0100

    new cell class
    used to build the random maze

---
+++ maze/logic/Cell.java
@@ -0,0 +1,14 @@
+package maze.logic;
+
+public class Cell extends Elements{
+	
+	public Cell(){
+		this.set_x(0);
+		this.set_y(0);
+	}
+	
+	public Cell(int x, int y){
+		this.set_x(x);
+		this.set_y(y);
+	}
+}
