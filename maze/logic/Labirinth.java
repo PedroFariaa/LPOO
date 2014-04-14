8947619835f11641570b221d69b970e0493eba85
PedroFariaa <pedrommlcfaria@gmail.com>
2014-04-14 23:50:46 +0100

    Random maze working

--- maze/logic/Labirinth.java
+++ maze/logic/Labirinth.java
@@ -1,14 +1,19 @@
 package maze.logic;
 
+import java.util.Random;
+import java.util.Stack;
+
 public class Labirinth{
-	
+
 	char[][]labirinth;
-	
+	int dim;
+
 	public Labirinth(){
-		this.labirinth=this.Default_maze();
+		this.labirinth=this.Random_maze(15);
 	}
-	
+
 	public char[][] Default_maze(){
+		dim = 10;
 		char[][] labirinth = {
 				{'X','X','X','X','X','X','X','X','X','X'},
 				{'X',' ',' ',' ',' ',' ',' ',' ',' ','X'},
@@ -20,34 +25,28 @@ public class Labirinth{
 				{'X',' ','X','X',' ','X',' ','X',' ','X'},
 				{'X',' ','X','X',' ',' ',' ',' ',' ','X'},
 				{'X','X','X','X','X','X','X','X','X','X'}
-				};
+		};
 		return labirinth;
 	}
 
 	public char[][] Random_maze(int size){
-		int size1, size2;
-		size1=size; size2=size;
-		this.labirinth = new char[size1][size2];
-		Elements path = new Elements();
-		
-		//exterior Wall
-		for(int i=0; i<size1-1; i++){
-			labirinth[i][0]='X';
-			labirinth[i][size-1]='X';
-		}
-		for(int i=0; i<size1-1; i++){
-			labirinth[0][i]='X';
-			labirinth[size-1][i]='X';
-		}		
+		dim=size;
+		this.labirinth = new char[size][size];
+		boolean[][] visitedCells = new boolean[size-2][size-2];
 
-		//fills with Walls
-		for(int i=1; i<size-2; i++){
-			for(int j=1; j<size-2; j++){
-				labirinth[i][j]='X';
+
+		// filling labyrinth with X
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				if (i % 2 != 0 && j % 2 != 0)
+					labirinth[i][j] = ' ';
+				else
+					labirinth[i][j] = 'X';
 			}
 		}
-		
-		//places Exit
+
+		// generating exit
+		int exitX, exitY;
 		int direction= (int)(Math.random() * 2); //0->horizontal    1->vertical
 		int exit1= (int)(Math.random() * 2);
 		int exit2= (int)(Math.random() * size+1);
@@ -55,168 +54,137 @@ public class Labirinth{
 			if(exit1==0){
 				labirinth[0][exit2]='S';
 				labirinth[1][exit2]=' ';
-				path.set_x(1);
-				path.set_y(exit2);
+				exitX = 0;
+				exitY = exit2;
 			}else{
 				labirinth[size-1][exit2]='S';
 				labirinth[size-2][exit2]=' ';
-				path.set_x(size-2);
-				path.set_y(exit2);
+				exitX = size-1;
+				exitY = exit2;
 			}
 		}
 		else{
 			if(exit1==0){
 				labirinth[exit2][0]='S';
 				labirinth[exit2][1]=' ';
-				path.set_x(exit2);
-				path.set_y(1);
+				exitX = exit2;
+				exitY = 0;
 			}else{
 				labirinth[exit2][size-1]='S';
 				labirinth[exit2][size-2]=' ';
-				path.set_x(exit2);
-				path.set_y(size-2);
+				exitX = size-1;
+				exitY = 0;
 			}
 		}
-		
-		
-		//builds path
-	//	while(parede(size)==true){
-			int move= (int)(Math.random() * 6);
-			for(int i=1; i<size-4; i++){
-				for(int j=1; j<size-4; j++){
-					if(labirinth[i][j]=='X' && labirinth[i+1][j]=='X' && labirinth[i+2][j]=='X' && labirinth[i][j+1]=='X' && 
-							labirinth[i][j+2]=='X' && labirinth[i+1][j+1]=='X' && labirinth[i+2][j+1]=='X' && labirinth[i+1][j+2]=='X' && 
-							labirinth[i+2][j+2]=='X'){
-						switch(move){
-						//horizontal
-						case 0:
-							labirinth[i+1][j+1]=' ';
-							int a=0;
-							while(labirinth[i+a][j+1] != ' ' && i+a<size-1){
-								labirinth[i+a][j+1]=' ';
-								a++;
-							}
-							break;
-						//vertical
-						case 1:
-							labirinth[i+1][j+1]=' ';
-							a=0;
-							while(labirinth[i+1][j+a] != ' ' && j+a<size-1){
-								labirinth[i+1][j+a]=' ';
-								a++;
-							}
-							break;
-						// |_
-						case 2:
-							labirinth[i+1][j+1]=' ';
-							a=0; int b=0;
-							while(labirinth[i+a][j+1] != ' ' && i+a<size-1){
-								labirinth[i+a][j+1]=' ';
-								a++;
-							}
-							while(labirinth[i+1][j+b] != ' ' && j+b<size-1){
-								labirinth[i+1][j+b]=' ';
-								b++;
-							}
-							break;
-						// case _|
-						case 3:
-							labirinth[i+1][j+1]=' ';
-							a=0;b=0;
-							while(labirinth[i-a][j+1] != ' ' && i-a>1){
-								labirinth[i-a][j+1]=' ';
-								a++;
-							}
-							while(labirinth[i+1][j+b] != ' ' && j+b<size-1){
-								labirinth[i+1][j+b]=' ';
-								b++;
-							}
-							break;
-							// -|
-						case 4:
-							labirinth[i+1][j+1]=' ';
-							a=0;b=0;
-							while(labirinth[i-a][j+1] != ' ' && i-a>1){
-								labirinth[i-a][j+1]=' ';
-								a++;
-							}
-							while(labirinth[i+1][j-b] != ' ' && j+b>1){
-								labirinth[i+1][j-b]=' ';
-								b++;
-							}
-							break;
-						case 5:
-							labirinth[i+1][j+1]=' ';
-							a=0;b=0;
-							while(labirinth[i+a][j+1] != ' ' && i+a<size-1){
-								labirinth[i+a][j+1]=' ';
-								a++;
-							}
-							while(labirinth[i+1][j-b] != ' ' && j+b>1){
-								labirinth[i+1][j-b]=' ';
-								b++;
-							}
-							break;				
-						}
-					}	
-				}
-			}
-	//	}
-		
-	//	while(livre(size)==true){
-			move= (int)(Math.random() * 4);
-			for(int i=1; i<size-3; i++){
-				for(int j=1; j<size-3; j++){
-					if(labirinth[i][j]==' ' && labirinth[i+1][j]==' ' && labirinth[i][j+1]==' ' && labirinth[i+1][j+1]==' '){
-						switch(move){
-						case 0:
-							labirinth[i][j]='X';
-							break;
-						case 1:
-							labirinth[i+1][j]='X';
-							break;
-						case 2:
-							labirinth[i][j+1]='X';
-							break;
-						case 3:
-							labirinth[i+1][j+1]='X';
-							break;
-						}
-					}	
-				}
-			}
-	//	}
-			return labirinth;
-	}
-/*
-	//checks if there is any 3x3 block with wall
-	boolean parede(int size){
-		boolean res = false;
-		for(int i=1; i<size-4; i++){
-			for(int j=1; j<size-4; j++){
-				if(labirinth[i][j]=='X' && labirinth[i+1][j]=='X' && labirinth[i+2][j]=='X' && labirinth[i][j+1]=='X' && 
-						labirinth[i][j+2]=='X' && labirinth[i+1][j+1]=='X' && labirinth[i+2][j+1]=='X' && labirinth[i+1][j+2]=='X' && 
-						labirinth[i+2][j+2]=='X'){
-					res = true;
+
+		// preparing matrix for maze generation
+		for (int i = 0; i < visitedCells.length; i++)
+			for (int j = 0; j < visitedCells[i].length; j++)
+				visitedCells[i][j] = false;
+
+
+		// preparing data for maze generation
+		Cell cellNextToExit = new Cell(exitX, exitY);
+		if (exitX == 0)
+			cellNextToExit.set_x(cellNextToExit.get_x() + 1);
+		else if (exitX == size - 1)
+			cellNextToExit.set_x(cellNextToExit.get_x() - 1);
+		else if (exitY == 0)
+			cellNextToExit.set_y(cellNextToExit.get_y() + 1);
+		else
+			cellNextToExit.set_y(cellNextToExit.get_y() - 1);
+
+		int guideCellX = (cellNextToExit.get_x() - 1) / 2;
+		int guideCellY = (cellNextToExit.get_y() - 1) / 2;
+		Cell guideCell = new Cell(guideCellX, guideCellY);
+
+		visitedCells[guideCell.get_y()][guideCell.get_x()] = true;
+		Stack<Cell> pathHistory = new Stack<Cell>();
+		pathHistory.push(new Cell(guideCell.get_x(), guideCell.get_y()));
+
+		// running maze generation algorithm
+		while (!pathHistory.empty()) {
+			// if all neighbors of guideCell have been visited
+			if ((guideCell.get_x()+1 >= (size-1)/2 || visitedCells[guideCell.get_y()][guideCell.get_x()+1] == true)
+					&& (guideCell.get_x()-1 < 0 || visitedCells[guideCell.get_y()][guideCell.get_x()-1] == true) &&
+					(guideCell.get_y()+1 >= (size-1)/2 || visitedCells[guideCell.get_y()+1][guideCell.get_x()] == true)
+					&& (guideCell.get_y()-1 < 0 || visitedCells[guideCell.get_y()-1][guideCell.get_x()] == true)) {
+
+				// pop cell from history
+				pathHistory.pop();
+
+				// if path history is empty, terminate algorithm
+				if (pathHistory.empty())
 					break;
-				}	
+				else
+					guideCell = pathHistory.peek();
+			}
+
+			// generating a new path
+			Random r = new Random();
+			switch (r.nextInt(4)) {  // see this after the cases
+			// up
+			case 0:
+				if (guideCell.get_y()-1 >= 0
+				&& visitedCells[guideCell.get_y()-1][guideCell.get_x()] == false) {
+					// updating maze
+					labirinth[guideCell.get_y()*2][guideCell.get_x()*2+1] = ' ';
+
+					// updating guideCell position
+					guideCell.set_y(guideCell.get_y()-1);
+					pathHistory.push(new Cell(guideCell.get_x(), guideCell.get_y()));
+
+					// marking cell as visited
+					visitedCells[guideCell.get_y()][guideCell.get_x()] = true;
+				}
+				break;
+				// right
+			case 1:
+				if (guideCell.get_x()+1 < (size-1)/2 && visitedCells[guideCell.get_y()][guideCell.get_x()+1] == false){
+					// updating maze
+					labirinth[guideCell.get_y()*2+1][(guideCell.get_x()+1)*2] = ' ';
+
+					// updating guideCell position
+					guideCell.set_x(guideCell.get_x()+1);
+					pathHistory.push(new Cell(guideCell.get_x(), guideCell.get_y()));
+
+					// marking cell as visited
+					visitedCells[guideCell.get_y()][guideCell.get_x()] = true;
+				}
+				break;
+				// down
+			case 2:
+				if (guideCell.get_y()+1 < (size-1)/2 && visitedCells[guideCell.get_y()+1][guideCell.get_x()] == false){
+					// updating maze
+					labirinth[(guideCell.get_y()+1)*2][guideCell.get_x()*2+1] = ' ';
+
+					// updating guideCell position
+					guideCell.set_y(guideCell.get_y()+1);
+					pathHistory.push(new Cell(guideCell.get_x(), guideCell.get_y()));
+
+					// marking cell as visited
+					visitedCells[guideCell.get_y()][guideCell.get_x()] = true;
+				}
+				break;
+				// left
+			case 3:
+				if (guideCell.get_x()-1 >= 0 && visitedCells[guideCell.get_y()][guideCell.get_x()-1] == false){
+					// updating maze
+					labirinth[guideCell.get_y()*2+1][guideCell.get_x()*2] = ' ';
+
+					// updating guideCell position
+					guideCell.set_x(guideCell.get_x()-1);
+					pathHistory.push(new Cell(guideCell.get_x(), guideCell.get_y()));
+
+					// marking cell as visited
+					visitedCells[guideCell.get_y()][guideCell.get_x()] = true;
+				}
+				break;
 			}
 		}
-		return res;
+
+		return labirinth;
 	}
-	
-	////checks if there is any 2x2 block without anything
-	boolean livre(int size){
-		boolean res = false;
-		for(int i=1; i<size-3; i++){
-			for(int j=1; j<size-3; j++){
-				if(labirinth[i][j]==' ' && labirinth[i+1][j]==' ' && labirinth[i][j+1]==' ' && labirinth[i+1][j+1]==' '){
-					res = true;
-					break;
-				}	
-			}
-		}
-		return res;
-	}*/
-	
+
+
 }
