b52fef313b0d3e3400dac00dded44d4ed6bbb8b6
PedroFariaa <pedrommlcfaria@gmail.com>
2014-04-18 20:15:27 +0100

    Test files
    teste implemented and running 100%

---
+++ maze/test/dragon_tests.java
@@ -0,0 +1,147 @@
+package maze.test;
+
+import static org.junit.Assert.*;
+
+import java.util.Vector;
+
+import maze.logic.Dragon;
+import maze.logic.Eagle;
+import maze.logic.Elements;
+import maze.logic.Game;
+import maze.logic.Hero;
+import maze.logic.Labyrinth;
+import maze.logic.Sword;
+
+import org.junit.Test;
+
+public class dragon_tests {
+
+	Labyrinth lab = new Labyrinth();
+	Hero heroi = new Hero(lab);
+	Sword espada = new Sword(lab);
+	Dragon d = new Dragon(lab);
+	Eagle bird = new Eagle(lab, heroi);
+	Vector<Dragon> drag = new Vector<Dragon> ();
+	Elements exit = new Elements();
+	Game g = new Game(lab, espada, heroi, exit, drag);
+
+	@Test
+	public void test_several_dragons(){
+		g.InitializeDragons(4);
+
+		assertEquals(4, drag.size());
+	}
+
+	@Test
+	public void test_dragon_sleeps(){
+		g.InitializeDragons(3);
+		drag.get(0).set_x(8);
+		drag.get(0).set_y(6);
+		drag.get(1).set_x(8);
+		drag.get(1).set_y(8);
+		drag.get(2).set_x(1);
+		drag.get(2).set_y(8);
+
+		while(!drag.get(0).get_sleeping() || !drag.get(1).get_sleeping() || !drag.get(2).get_sleeping()){
+			for(int i=0; i<3; i++){
+				drag.get(i).movement_sleep();
+			}
+		}
+		
+		assertEquals(true, ( drag.get(0).get_sleeping()==true || drag.get(1).get_sleeping()==true || drag.get(2).get_sleeping()==true ));
+	}
+	
+	@Test
+	public void test_dragon_doesnt_sleep(){
+		g.InitializeDragons(3);
+		drag.get(0).set_x(8);
+		drag.get(0).set_y(6);
+		drag.get(1).set_x(8);
+		drag.get(1).set_y(8);
+		drag.get(2).set_x(1);
+		drag.get(2).set_y(8);
+		boolean test = true;
+
+		for(int i=0; i<3; i++){
+			for(int j=0; j<100; j++){
+				drag.get(i).movement();
+				if(drag.get(i).get_sleeping()){
+					test=false;
+					break;
+				}
+			}
+		}
+		assertEquals(true, test);
+	}
+
+	@Test
+	public void test_AwakenDragon_Kills(){
+		g.InitializeDragons(2);
+		
+		heroi.set_x(1);
+		heroi.set_y(7);
+		drag.get(0).set_x(1);
+		drag.get(0).set_y(6);
+		drag.get(1).set_x(8);
+		drag.get(1).set_y(1);
+		
+		heroi.setArmado(false);
+		drag.get(0).set_sleeping(false);
+		drag.get(1).set_sleeping(false);
+		
+		heroi.set_x(drag.get(0).get_x());
+		heroi.set_y(drag.get(0).get_y());
+		g.CheckPositions();
+
+		assertEquals(false, heroi.get_alive());
+	}
+	
+	@Test
+	public void test_SleepingDragon_doesntKills(){
+		g.InitializeDragons(2);
+		
+		heroi.set_x(1);
+		heroi.set_y(7);
+		drag.get(0).set_x(1);
+		drag.get(0).set_y(6);
+		drag.get(1).set_x(8);
+		drag.get(1).set_y(1);
+		
+		heroi.setArmado(false);
+		drag.get(0).set_sleeping(true);
+		drag.get(1).set_sleeping(true);
+		
+		heroi.set_x(drag.get(0).get_x());
+		heroi.set_y(drag.get(0).get_y());
+		g.CheckPositions();
+
+		assertEquals(true, heroi.get_alive());
+	}
+	
+	@Test
+	public void test_sleepingAndAwakenDragons_die(){
+		g.InitializeDragons(2);
+		
+		heroi.set_x(1);
+		heroi.set_y(7);
+		drag.get(0).set_x(1);
+		drag.get(0).set_y(6);
+		drag.get(1).set_x(8);
+		drag.get(1).set_y(1);
+		
+		heroi.setArmado();
+		drag.get(0).set_sleeping(true);
+		drag.get(1).set_sleeping(false);
+		
+		heroi.set_x(drag.get(0).get_x());
+		heroi.set_y(drag.get(0).get_y());
+		g.CheckPositions();
+		
+		heroi.set_x(drag.get(1).get_x());
+		heroi.set_y(drag.get(1).get_y());
+		g.CheckPositions();
+
+		assertEquals(false, (drag.get(0).get_alive() && drag.get(1).get_alive()));
+		assertEquals(true, heroi.get_alive());
+	}
+}
---
+++ maze/test/hero_tests.java
@@ -0,0 +1,182 @@
+package maze.test;
+
+import java.util.Vector;
+
+import maze.logic.*;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class hero_tests {
+	Labyrinth lab = new Labyrinth();
+	Hero heroi = new Hero(lab);
+	Sword espada = new Sword(lab);
+	Dragon d = new Dragon(lab);
+	Eagle bird = new Eagle(lab, heroi);
+	Vector<Dragon> drag = new Vector<Dragon> ();
+	Elements exit = new Elements();
+	Game g = new Game(lab, espada, heroi, exit, drag);
+	
+	@Test
+	public void test_MoveToFreeCell(){
+		heroi.set_x(3);
+		heroi.set_y(1);
+		espada.set_x(1);
+		espada.set_y(8);
+		d.set_x(8);
+		d.set_y(8);
+				
+		heroi.movement("s", bird);
+		heroi.movement("s", bird);
+		assertEquals(5,heroi.get_x());
+		assertEquals(1,heroi.get_y());
+		
+		heroi.movement("d", bird);
+		heroi.movement("d", bird);
+		heroi.movement("d", bird);
+		assertEquals(5,heroi.get_x());
+		assertEquals(4,heroi.get_y());
+		
+		heroi.movement("w", bird);
+		assertEquals(4,heroi.get_x());
+		assertEquals(4,heroi.get_y());
+		
+		heroi.movement("s", bird);
+		heroi.movement("a", bird);
+		heroi.movement("a", bird);
+		assertEquals(5,heroi.get_x());
+		assertEquals(2,heroi.get_y());
+	}
+	
+	
+	@Test
+	public void test_MoveAgainstWall(){
+		heroi.set_x(5);
+		heroi.set_y(6);
+		espada.set_x(1);
+		espada.set_y(8);
+		d.set_x(8);
+		d.set_y(8);
+				
+		heroi.movement("d", bird);
+		assertEquals(5,heroi.get_x());
+		assertEquals(6,heroi.get_y());
+		
+		heroi.movement("a", bird);
+		heroi.movement("w", bird);
+		assertEquals(5,heroi.get_x());
+		assertEquals(5,heroi.get_y());
+		
+		heroi.movement("s", bird);
+		assertEquals(5,heroi.get_x());
+		assertEquals(5,heroi.get_y());
+		
+		heroi.movement("a", bird);
+		heroi.movement("w", bird);
+		heroi.movement("a", bird);
+		assertEquals(4,heroi.get_x());
+		assertEquals(4,heroi.get_y());
+	}
+	
+	@Test
+	public void test_EquipSword(){
+		heroi.set_x(1);
+		heroi.set_y(1);
+		espada.set_x(1);
+		espada.set_y(3);
+		d.set_x(8);
+		d.set_y(8);
+		
+		heroi.movement("d", bird);
+		heroi.movement("d", bird);
+		g.CheckPositions();
+		heroi.movement("d", bird);		
+		
+		assertEquals(true, heroi.getArmado());
+		assertEquals(true, espada.get_equiped());
+	}
+	
+	@Test
+	public void test_GettingKilled(){
+		heroi.set_x(1);
+		heroi.set_y(1);
+		espada.set_x(8);
+		espada.set_y(8);
+		g.InitializeDragons(1);
+		drag.get(0).set_x(1);
+		drag.get(0).set_y(3);
+		
+		heroi.setArmado(false);
+		heroi.movement("d", bird);
+		drag.get(0).set_x(1);
+		drag.get(0).set_y(3);
+		drag.get(0).wake();
+		g.CheckPositions();
+		heroi.movement("d", bird);
+		
+		assertEquals(false, heroi.get_alive());
+	}
+	
+	@Test
+	public void test_KillDragon(){
+		heroi.set_x(1);
+		heroi.set_y(1);
+		espada.set_x(8);
+		espada.set_y(8);
+		g.InitializeDragons(1);
+		drag.get(0).set_x(1);
+		drag.get(0).set_y(3);
+		
+		heroi.setArmado(true);
+		heroi.movement("d", bird);
+		drag.get(0).set_x(1);
+		drag.get(0).set_y(3);
+		drag.get(0).wake();
+		g.CheckPositions();
+		heroi.movement("d", bird);
+		
+		assertEquals(true, heroi.get_alive());
+		assertEquals(false, drag.get(0).get_alive());
+	}
+	
+	@Test
+	public void test_WinGame(){
+		heroi.set_x(1);
+		heroi.set_y(7);
+		espada.set_x(2);
+		espada.set_y(8);
+		g.InitializeDragons(1);
+		drag.get(0).set_x(8);
+		drag.get(0).set_y(1);
+		
+		heroi.movement("d", bird);
+		heroi.movement("s", bird);
+		g.CheckPositions();
+		
+		assertEquals(true, heroi.getArmado());
+		
+		heroi.movement("s", bird);
+		drag.get(0).set_x(4);
+		drag.get(0).set_y(8);
+		g.CheckPositions();
+		
+		assertEquals(false, drag.get(0).get_alive());
+		
+		heroi.movement("s", bird);
+		heroi.movement("d", bird);
+		
+		assertEquals(true, g.EndGame());
+	}
+	
+	@Test
+	public void CantExit(){
+		heroi.set_x(4);
+		heroi.set_y(8);
+		
+		assertEquals(false, heroi.getArmado());
+		
+		heroi.movement("d", bird);
+		
+		assertEquals(false, g.EndGame());
+	}
+	
+}
