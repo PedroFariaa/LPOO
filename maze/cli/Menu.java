0069ed50ac7e2062147bb6dcdb6130bcdf6f277b
PedroFariaa <pedrommlcfaria@gmail.com>
2014-04-18 20:11:37 +0100

    New comand line interface Menu
    Simplier but with a few bugs

--- maze/cli/Menu.java
+++ maze/cli/Menu.java
@@ -1,47 +1,37 @@
 package maze.cli;
 import java.util.Scanner;
 
-import maze.logic.Dragon;
 import maze.logic.Game;
 
 public class Menu {
 
-	public void Mostra_menu(){
-		System.out.println("1 - Play Default Mode with one dragon");
-		System.out.println("1 - Play Regular Mode");
-		System.out.println("1 - Play Random Mode with one dragon");
-		System.out.println("1 - Play Regular Mode");
-	}
-
-	public static void main(String[] argc){
-		/*	Menu m = new Menu();
-		m.Mostra_menu();		
-		String userInput;
-		Scanner scan = new Scanner (System.in);
-		userInput = scan.nextLine();
-		switch(userInput){
-		case "1":
-			// Play Game
-			Game g = new Game();
-		/*	int n_dragons;
-		  	System.out.println("Chose the number of dragons you expect to find");
-			Scanner scan2 = new Scanner (System.in);
-			n_dragons = scan2.nextInt();
-			g.CreatesDragons(n_dragons);*/
-		/*
-			g.PlayGame();
-			break;
-		case "2":
-			//
-			break;
-		}*/
-		Game g = new Game();
+	public void Number_dragons(Game g){
 		int n_dragons;
-		System.out.println("Chose the number of dragons you expect to find");
+		System.out.println("choose the number of dragons you expect to find");
 		Scanner scan2 = new Scanner (System.in);
 		n_dragons = scan2.nextInt();
 		g.InitializeDragons(n_dragons);
-		g.PlayGame();
+	}
+
+	public boolean SleepingDragon(Game g){
+		String s; boolean res;
+		System.out.println("Choose Y if you want your dragon to sleep or N if you don't");
+		Scanner scan2 = new Scanner (System.in);
+		s = scan2.next();
+		if(s == "N" || s == "n"){
+			res = false;
+		}else{
+			res = true;
+		}
+		return res;
+	}
+
+	public static void main(String[] argc){
+		Menu m = new Menu();
+		Game g1 = new Game();
+		m.Number_dragons(g1);
+		boolean sleep=m.SleepingDragon(g1);
+		g1.PlayGame(sleep);
 	}
 
 }
