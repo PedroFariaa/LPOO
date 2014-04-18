6c6eea3a46897572e95bd8feff7d1f78d4924966
PedroFariaa <pedrommlcfaria@gmail.com>
2014-04-18 20:14:25 +0100

    Bugs in CheckPosition method

--- maze/logic/Game.java
+++ maze/logic/Game.java
@@ -5,8 +5,8 @@ import java.util.Vector;
 
 public class Game{
 
-	
-	Labirinth lab = new Labirinth();
+
+	Labyrinth lab = new Labyrinth();
 	private Vector<Dragon> drag = new Vector<Dragon>();
 	Sword espada = new Sword(lab);
 	Hero heroi = new Hero(lab);
@@ -14,21 +14,22 @@ public class Game{
 	Display dis = new Display();
 	Elements exit = new Elements();
 	boolean FoundExit=false;
-	
-	
+
+	public Game(int n){
+		this.lab = new Labyrinth(n);
+	}
+
+	public Game(Labyrinth lab, Sword espada, Hero heroi, Elements exit, Vector<Dragon> drag){
+		this.lab=lab;
+		this.espada=espada;
+		this.heroi=heroi;
+		this.exit=exit;
+		this.dis = new Display();
+		this.drag = drag;
+	}
 
 	public boolean EndGame(){
 		boolean end=false;
-		/*
-		if(drag.get_alive()==false){
-			end=true;
-		}else{
-			if(!heroi.get_alive()){
-				end=true;
-			}
-		}
-		return end;
-		 */
 		for(int i=0; i<drag.size(); i++){
 			if(drag.get(i).get_alive()==false){
 				end=true;
@@ -43,11 +44,11 @@ public class Game{
 		return end;
 	}
 
-	public void CheckPositions(){  // ALTERAR FUNÇAO PARA FUNCIONAR COM DRAGONS MULTIPLOS
+	public void CheckPositions(){
 		//verifica dragao
 		for(int i=0; i<drag.size(); i++){
-			if(!drag.get(i).get_asleep()){
-				if((lab.labirinth[heroi.get_x()+1][heroi.get_y()] == lab.labirinth[drag.get(i).get_x()][drag.get(i).get_y()]) || (lab.labirinth[heroi.get_x()-1][heroi.get_y()] == lab.labirinth[drag.get(i).get_x()][drag.get(i).get_y()]) || (lab.labirinth[heroi.get_x()][heroi.get_y()+1] == lab.labirinth[drag.get(i).get_x()][drag.get(i).get_y()]) || (lab.labirinth[heroi.get_x()][heroi.get_y()-1] == lab.labirinth[drag.get(i).get_x()][drag.get(i).get_y()]) || (lab.labirinth[heroi.get_x()][heroi.get_y()] == lab.labirinth[drag.get(i).get_x()][drag.get(i).get_y()])){
+			if(!drag.get(i).get_sleeping()){
+				if((lab.getLabyrinth()[heroi.get_x()+1][heroi.get_y()] == lab.getLabyrinth()[drag.get(i).get_x()][drag.get(i).get_y()]) || (lab.getLabyrinth()[heroi.get_x()-1][heroi.get_y()] == lab.getLabyrinth()[drag.get(i).get_x()][drag.get(i).get_y()]) || (lab.getLabyrinth()[heroi.get_x()][heroi.get_y()+1] == lab.getLabyrinth()[drag.get(i).get_x()][drag.get(i).get_y()]) || (lab.getLabyrinth()[heroi.get_x()][heroi.get_y()-1] == lab.getLabyrinth()[drag.get(i).get_x()][drag.get(i).get_y()]) || (lab.getLabyrinth()[heroi.get_x()][heroi.get_y()] == lab.getLabyrinth()[drag.get(i).get_x()][drag.get(i).get_y()])){
 					if(heroi.getArmado()){
 						drag.get(i).killDragon();
 					}else{
@@ -56,13 +57,14 @@ public class Game{
 					}
 				}
 			}else{
-				if((lab.labirinth[heroi.get_x()+1][heroi.get_y()] == lab.labirinth[drag.get(i).get_x()][drag.get(i).get_y()]) || (lab.labirinth[heroi.get_x()-1][heroi.get_y()] == lab.labirinth[drag.get(i).get_x()][drag.get(i).get_y()]) || (lab.labirinth[heroi.get_x()][heroi.get_y()+1] == lab.labirinth[drag.get(i).get_x()][drag.get(i).get_y()]) || (lab.labirinth[heroi.get_x()][heroi.get_y()-1] == lab.labirinth[drag.get(i).get_x()][drag.get(i).get_y()]) || (lab.labirinth[heroi.get_x()][heroi.get_y()] == lab.labirinth[drag.get(i).get_x()][drag.get(i).get_y()])){
+				if((lab.getLabyrinth()[heroi.get_x()+1][heroi.get_y()] == lab.getLabyrinth()[drag.get(i).get_x()][drag.get(i).get_y()]) || (lab.getLabyrinth()[heroi.get_x()-1][heroi.get_y()] == lab.getLabyrinth()[drag.get(i).get_x()][drag.get(i).get_y()]) || (lab.getLabyrinth()[heroi.get_x()][heroi.get_y()+1] == lab.getLabyrinth()[drag.get(i).get_x()][drag.get(i).get_y()]) || (lab.getLabyrinth()[heroi.get_x()][heroi.get_y()-1] == lab.getLabyrinth()[drag.get(i).get_x()][drag.get(i).get_y()]) || (lab.getLabyrinth()[heroi.get_x()][heroi.get_y()] == lab.getLabyrinth()[drag.get(i).get_x()][drag.get(i).get_y()])){
 					if(heroi.getArmado()){
 						drag.get(i).killDragon();
 					}
 				}
 			}
 		}
+
 
 		//verifica espada
 		if(!heroi.getArmado()){
@@ -73,6 +75,18 @@ public class Game{
 		}
 
 		//verifica passaro
+		if(!heroi.getArmado()){
+			if(bird.get_x()==espada.get_x() && bird.get_y()==espada.get_y()){
+				bird.setTravelling(true);
+			}
+		}
+		if(bird.get_alive()){
+			for(int i=0; i<drag.size(); i++){
+				if(!drag.get(i).get_sleeping() && drag.get(i).get_x()==bird.get_x() && drag.get(i).get_y()==bird.get_y()){
+					bird.KillsEagle(espada);
+				}
+			}
+		}
 
 	}
 
@@ -81,28 +95,33 @@ public class Game{
 		bird.ShowEagle(lab);
 		heroi.ShowHero(lab);
 		for(int i=0; i<drag.size(); i++){
-			drag.get(i).ShowDragon(lab);
+			drag.get(i).ShowDragon(lab, espada);
 		}
 	}
 
 
-	public void PlayGame(){
+	public void PlayGame(boolean sleepingDragon){
 		while(EndGame() == false){
 			dis.DisplayMap(lab, heroi, drag, espada, bird);
 			String userInput;
 			Scanner scan = new Scanner (System.in);
 			userInput = scan.nextLine();
 			for(int i=0; i<drag.size(); i++){
-				drag.get(i).movement();
+				if(sleepingDragon==true){
+					drag.get(i).movement_sleep();
+					CheckPositions();
+				}else{
+					drag.get(i).movement();
+					CheckPositions();
+				}
 			}
 			CheckPositions();
 			heroi.movement(userInput, bird);
 			CheckPositions();
-			if(bird.getTravelling()==true){
+			if(bird.getTravelling()){
 				bird.Movement(espada);
-				bird.ShowEagle(lab);
 			}
-			//CheckPositions_eagle();
+			CheckPositions();
 		}
 		while(FoundExit == false){
 			dis.DisplayMap(lab, heroi, drag, espada, bird);
@@ -110,7 +129,13 @@ public class Game{
 			Scanner scan = new Scanner (System.in);
 			userInput = scan.nextLine();
 			for(int i=0; i<drag.size(); i++){
-				drag.get(i).movement();
+				if(sleepingDragon==true){
+					drag.get(i).movement_sleep();
+					CheckPositions();
+				}else{
+					drag.get(i).movement();
+					CheckPositions();
+				}
 			}
 			CheckPositions();
 			heroi.movement(userInput, bird);
@@ -121,30 +146,30 @@ public class Game{
 
 
 	private void CheckExit() {
-		for(int i=0; i < lab.labirinth.length; i++){
-			for(int j=0; j < lab.labirinth.length; j++){
-				if(lab.labirinth[i][j] == 'S'){
+		for(int i=0; i < lab.getLabyrinth().length; i++){
+			for(int j=0; j < lab.getLabyrinth().length; j++){
+				if(lab.getLabyrinth()[i][j] == 'S'){
 					exit.set_x(i);
 					exit.set_y(j);
 					break;
 				}
 			}
 		}
-		if(lab.labirinth[exit.get_x()][exit.get_y()]=='A'){
-			lab.labirinth[heroi.get_x()][heroi.get_y()] = ' ';
-			lab.labirinth[exit.get_x()][exit.get_y()] = 'A';
+		if(lab.getLabyrinth()[exit.get_x()][exit.get_y()]=='A'){
+			lab.getLabyrinth()[heroi.get_x()][heroi.get_y()] = ' ';
+			lab.getLabyrinth()[exit.get_x()][exit.get_y()] = 'A';
 			dis.DisplayMap(lab, heroi, drag, espada, bird);
-			System.out.println("You are at least free!");
+			System.out.println("You are free at least!");
 			FoundExit = true;
 		}
 	}
-	
+
 	public void InitializeDragons(int n){
 		for(int i=0; i<n; i++){
 			drag.add(new Dragon(lab));
 		}
 	}
-	
+
 
 }
 
