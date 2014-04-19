924c8161a52410f19add35c5cb0babb35766f573
PedroFariaa <pedrommlcfaria@gmail.com>
2014-04-19 17:17:58 +0100

    Dragon bug fixed

--- maze/cli/Menu.java
+++ maze/cli/Menu.java
@@ -5,20 +5,21 @@ import maze.logic.Game;
 
 public class Menu {
 
-	public void Number_dragons(Game g){
+	Scanner scan = new Scanner(System.in);
+	boolean random = false;
+	
+	private void Number_dragons(Game g){
 		int n_dragons;
 		System.out.println("choose the number of dragons you expect to find");
-		Scanner scan2 = new Scanner (System.in);
-		n_dragons = scan2.nextInt();
+		n_dragons = scan.nextInt();
 		g.InitializeDragons(n_dragons);
 	}
 
-	public boolean SleepingDragon(Game g){
+	private boolean SleepingDragon(Game g){
 		String s; boolean res;
 		System.out.println("Choose Y if you want your dragon to sleep or N if you don't");
-		Scanner scan3 = new Scanner (System.in);
-		s = scan3.next();
-		if(s == "N" || s == "n"){
+		s = scan.next();
+		if(s.equals("N") || (s.equals("n"))){
 			res = false;
 		}else{
 			res = true;
@@ -26,8 +27,30 @@ public class Menu {
 		return res;
 	}
 
+//	private boolean RandomLab(boolean rand){
+//		String Slab;
+//		System.out.println("Do you want to play in a Random Labyrinth ?");
+//		Slab = scan.next();
+//		if (Slab.equals("y") || Slab.equals("Y")){
+//			rand = true;
+//		}
+//		return rand;
+//	}
+//	
+//	private int LabSize(){
+//		System.out.println("Tamanho do labirinto (tem de ser ímpar): ");
+//		int size = scan.nextInt();
+//		return size;
+//	}
+
 	public static void main(String[] argc){
+//		boolean rand = false;
+//		int s=11;
 		Menu m = new Menu();
+//		m.RandomLab(rand);
+//		if(rand==true){
+//			s = m.LabSize();
+//		}
 		Game g1 = new Game();
 		m.Number_dragons(g1);
 		boolean sleep = m.SleepingDragon(g1);
